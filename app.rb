3084fb59d771e8f13fbcf78fbefb34306d158c89
Corbyn Foster <corbyn.foster@yale.edu>
2020-04-22 06:55:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,14 +2,12 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 #
-require_relative '../app'
 name = "corbyn"
 age = 19
 favorite_food = "sushi"
 height_in_inches = 71
-AVERAGE_HEIGHT_IN_INCHES = 69
-below_average = height_in_inches < 69
-above_average = height_in_inches > 69
+below_average = height_in_inches < 67
+above_average = height_in_inches > 67
 height_in_inches ? 'You are taller than average.' : 'You are not taller than average.'
 ## Do not modify below this line!
 
